6184efddf9b84481cc2d257074f474bd4ca36346
German Guaigua <gerguaigua@gmail.com>
2021-02-25 16:51:44 -0300

    Revisando Array

---
+++ assets/js/scripts.js
@@ -0,0 +1,14 @@
+const hamburgerButton = document.getElementById('hamburger-button');
+const headerLinks = document.querySelectorAll('header .container .menu a');
+
+headerLinks.forEach((link) => {
+    link.addEventListener('click', (e) => hamburgerButton.checked = false);
+})
+
+var arr = [ 'a', 'b', 'c', 'd'];
+
+arr.forEach(function(element,index,arreglo){
+    console.log(element);
+    console.log(index);
+    console.log(arreglo);
+});
